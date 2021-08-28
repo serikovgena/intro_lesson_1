e130920fd387c7bd55f12c317fe58d67fb24b6a7
Serikov Gennadiy <genahost@mail.ru>
2021-08-28 21:24:41 +0300

    feat: add .gitignore, lesson 1 cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как тебя зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now.ToString("dd MMMM yyyy")} года.");
+        }
+    }
+}
